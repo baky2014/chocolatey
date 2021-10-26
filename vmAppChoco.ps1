178b6ad32282fb3434f62174c6fabeceb711b0df
baky2014 <51097490+baky2014@users.noreply.github.com>
2021-10-26 13:16:15 +0200

    add choco vm app install

---
+++ vmAppChoco.ps1
@@ -0,0 +1,10 @@
+﻿#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install visualstudiocode -y
+choco install git -y
+choco install nodejs-lts  -y
+choco install googlechrome -y
+choco install microsoftazurestorageexplorer -y
+choco install notepadplusplus -y
